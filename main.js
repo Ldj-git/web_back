79ace122fd5b9207b0cfa187e82426a9a8de6c1e
Ldj-git <68588092+Ldj-git@users.noreply.github.com>
2021-01-22 04:55:58 +0900

    string added
    스트링으로도 가능

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(bodyParser.text());
 app.post("/", function (req, res) {
   var input = req.body.num;
+  var output;
+  if (input[2] >= 3 && input[1] >= 4 && input[8] >= 4) {
+    output = 1;
+  } else if (input[2] >= 3 && input[1] >= 4 && input[8] <= 3) {
+    output = 2;
+  } else if (input[2] >= 3 && input[1] <= 3 && input[8] >= 4) {
+    output = 3;
+  } else if (input[2] >= 3 && input[1] <= 3 && input[8] <= 3) {
+    output = 4;
+  } else if (input[2] <= 2 && input[4] >= 4 && input[3] <= 3) {
+    output = 5;
+  } else if (input[2] <= 2 && input[4] >= 4 && input[3] >= 4) {
+    output = 6;
+  } else if (input[2] <= 2 && input[4] <= 3 && input[7] >= 4) {
+    output = 7;
+  } else if (input[2] <= 2 && input[4] <= 3 && input[7] <= 3) {
+    output = 8;
+  }
+  console.log(output);
+  res.json(output);
+});
+app.post("/string", function (req, res) {
+  var beforeparse = req.body;
+  beforeparse = beforeparse.slice(8, 27);
+  var afterparse = beforeparse.split(",");
+  var input = afterparse;
   var output;
   if (input[2] >= 3 && input[1] >= 4 && input[8] >= 4) {
     output = 1;
